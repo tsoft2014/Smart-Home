3408040bb67858a68ba907758838988d33562bc7
TSoft2014 <35295833+tsoft2014@users.noreply.github.com>
2026-09-17 16:42:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -741,7 +741,8 @@ class SmartHomeApp(App):
             background_normal='',
             background_color=(0, 0, 0, 0),
             color=(0.2, 0.85, 0.3, 1),
-            size_hint=(1, 1),
+            size_hint=(1, None),  # <--- Исправлено с (1, 1)
+            height=dp(40),  # <--- Задана высота
             halign='center',
             valign='middle'
         )
@@ -755,7 +756,8 @@ class SmartHomeApp(App):
             background_normal='',
             background_color=(0, 0, 0, 0),
             color=(0.9, 0.2, 0.2, 1),
-            size_hint=(1, 1),
+            size_hint=(1, None),  # <--- Исправлено с (1, 1)
+            height=dp(40),  # <--- Задана высота
             halign='center',
             valign='middle'
         )
